3533c344eb75c05d4ae8e6a227a99a638188e71c
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-02-20 18:28:54 +0530

    Extension webview started and fixing the css loading issue

--- extension.js
+++ extension.js
@@ -1,36 +1,94 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require("vscode");
+const path = require('path');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "intelliassist" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('intelliassist.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable1 = vscode.commands.registerCommand(
+		"intelliassist.helloWorld",
+		function () {
+			vscode.window.showInformationMessage("Hello World from IntelliAssist!");
+		}
+	);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from IntelliAssist!');
+	vscode.commands.registerCommand("extension.enableCustomPanel", () => {
+		let panel = vscode.window.createWebviewPanel(
+			"customPanel",
+			"Custom Panel",
+			vscode.ViewColumn.Beside,
+			{
+				// @ts-ignore
+				enableScripts: true,
+				// @ts-ignore
+				preserveFocus: true,
+				webviewOptions: {
+					// Set the initial width of the panel to 300px
+					width: 300
+				}
+			}
+		);
+
+		const highlightCssPath = vscode.Uri.file(path.join(context.extensionPath, 'node_modules', 'highlight.js', 'styles', 'default.css'));
+		const highlightCssUri = highlightCssPath.with({ scheme: 'vscode-webview-resource' });
+
+		const customCssPath = vscode.Uri.file(path.join(context.extensionPath, 'assets', 'styles.css'));
+		const customCssUri = customCssPath.with({ scheme: 'vscode-webview-resource' });
+
+		panel.webview.html = `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<link rel="stylesheet" href="${highlightCssUri.toString()}">
+			<link rel="stylesheet" href="${customCssUri.toString()}">
+			<title>Custom Panel</title>
+		</head>
+		<body>
+			<main>
+				<div class="view new-chat-view" style="margin: 0 10vw">
+					<div class="model-selector">
+
+					</div>
+					<div class="typewriter" style="height:60px">
+						Ask me anything !
+					</div>
+				</div>
+
+				<div id="message_container" class="view conversation-view">
+
+				</div>
+			</main>
+
+			<div id="message-form">
+				<div class="message-wrapper">
+					<textarea name="message" id="message" rows="1" placeholder="Ask Co-Pilot ..."
+						style="height:60px; max-height:170px; font-size:medium"></textarea>
+					<button onclick="apiCall()" type="submit" class="send-button">
+						<span id="submitButtonContent">
+							<i id="success-icon" class="fa fa-paper-plane"></i>
+						</span>
+					</button>
+					<i id="loading-image" class="fa fa-spinner" style="display: none;"></i>
+					<i id="success-icon" class="fa fa-paper-plane" style="display: none;"></i>
+				</div>
+			</div>
+		</body>
+    </html>
+        `;
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable1, vscode.commands.registerCommand('extension.enableMyExtension', () => {
+		vscode.commands.executeCommand('extension.enableCustomPanel');
+	}));
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
 	activate,
-	deactivate
-}
+	deactivate,
+};
