c095c3331a0d2754963ec618b195148adb1fcf51
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-02-21 18:20:23 +0530

    Added stylesheet and scripts

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 const vscode = require("vscode");
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -18,24 +19,41 @@ function activate(context) {
 		let panel = vscode.window.createWebviewPanel(
 			"customPanel",
 			"Custom Panel",
-			vscode.ViewColumn.Beside,
+			vscode.ViewColumn.Six,
 			{
-				// @ts-ignore
+				localResourceRoots: [
+					vscode.Uri.file(path.join(context.extensionPath, "assets")),
+					vscode.Uri.file(path.join(context.extensionPath, "node_modules")),
+				],
 				enableScripts: true,
 				// @ts-ignore
 				preserveFocus: true,
 				webviewOptions: {
-					// Set the initial width of the panel to 300px
-					width: 300
-				}
+					width: 300,
+				},
 			}
 		);
 
-		const highlightCssPath = vscode.Uri.file(path.join(context.extensionPath, 'node_modules', 'highlight.js', 'styles', 'default.css'));
-		const highlightCssUri = highlightCssPath.with({ scheme: 'vscode-webview-resource' });
+		const highlightCssPath = vscode.Uri.file(
+			path.join(
+				context.extensionPath,
+				"node_modules",
+				"highlight.js",
+				"styles",
+				"default.css"
+			)
+		);
+		const highlightCssSrc = panel.webview.asWebviewUri(highlightCssPath);
 
-		const customCssPath = vscode.Uri.file(path.join(context.extensionPath, 'assets', 'styles.css'));
-		const customCssUri = customCssPath.with({ scheme: 'vscode-webview-resource' });
+		const customCssPath = vscode.Uri.file(
+			path.join(context.extensionPath, "assets", "styles.css")
+		);
+		const customCssSrc = panel.webview.asWebviewUri(customCssPath);
+
+		const customJSPath = vscode.Uri.file(
+			path.join(context.extensionPath, "assets", "script.js")
+		);
+		const customJSSrc = panel.webview.asWebviewUri(customJSPath);
 
 		panel.webview.html = `
 		<!DOCTYPE html>
@@ -43,9 +61,12 @@ function activate(context) {
 		<head>
 			<meta charset="UTF-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<link rel="stylesheet" href="${highlightCssUri.toString()}">
-			<link rel="stylesheet" href="${customCssUri.toString()}">
 			<title>Custom Panel</title>
+			<link rel="stylesheet" href="${highlightCssSrc}">
+			<link rel="stylesheet" href="${customCssSrc}">
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+			integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+			crossorigin="anonymous" referrerpolicy="no-referrer" />
 		</head>
 		<body>
 			<main>
@@ -76,17 +97,31 @@ function activate(context) {
 					<i id="success-icon" class="fa fa-paper-plane" style="display: none;"></i>
 				</div>
 			</div>
+			<script src=${customJSSrc}></script>
+			<script src=${customJSSrc}></script>
 		</body>
     </html>
         `;
+
+		// Listen for the webview panel's event onDidChangeViewState
+		panel.onDidChangeViewState((e) => {
+			// Check if the webview is active and visible
+			if (panel.visible) {
+				// Trigger a layout update when the webview becomes visible
+				panel.webview.postMessage({ command: "updateLayout" });
+			}
+		});
 	});
 
-	context.subscriptions.push(disposable1, vscode.commands.registerCommand('extension.enableMyExtension', () => {
-		vscode.commands.executeCommand('extension.enableCustomPanel');
-	}));
+	context.subscriptions.push(
+		disposable1,
+		vscode.commands.registerCommand("extension.enableMyExtension", () => {
+			vscode.commands.executeCommand("extension.enableCustomPanel");
+		})
+	);
 }
 
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
